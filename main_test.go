092ad8c82c943e57c649f51bb4227aa9bb86e823
Sean Porter <portertech@gmail.com>
2022-03-14 15:27:04 -0700

    patch level bump, 0.2.2
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- main_test.go
+++ main_test.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"compress/gzip"
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"testing"
@@ -42,44 +39,4 @@ func TestExecuteHandler(t *testing.T) {
 	require.NoError(t, err)
 	handlerConfig.MetricsPort = port
 	assert.NoError(executeHandler(event))
-}
-
-func TestMain(t *testing.T) {
-	assert := assert.New(t)
-	file, _ := ioutil.TempFile(os.TempDir(), "sensu-wavefront-handler")
-	defer func() {
-		_ = os.Remove(file.Name())
-	}()
-
-	event := corev2.FixtureEvent("entity1", "check1")
-	event.Check = nil
-	event.Metrics = corev2.FixtureMetrics()
-	eventJSON, _ := json.Marshal(event)
-	_, err := file.WriteString(string(eventJSON))
-	require.NoError(t, err)
-	require.NoError(t, file.Sync())
-	_, err = file.Seek(0, 0)
-	require.NoError(t, err)
-	os.Stdin = file
-	requestReceived := false
-
-	var test = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestReceived = true
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(`{"ok": true}`))
-		require.NoError(t, err)
-	}))
-
-	url, err := url.ParseRequestURI(test.URL)
-	assert.NoError(err)
-	handlerConfig.Host = url.Hostname()
-	port, err := strconv.Atoi(url.Port())
-	require.NoError(t, err)
-	handlerConfig.MetricsPort = port
-	oldArgs := os.Args
-	os.Args = []string{"sensu-wavefront-handler", "--host", host, "--metrics-port", fmt.Sprint(port)}
-	defer func() { os.Args = oldArgs }()
-
-	main()
-	assert.True(requestReceived)
 }
